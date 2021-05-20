eeaf9084ac3006fd9be076975899879c49bcf2d6
Adi Levy <levyadi93@gmail.com>
2021-05-20 15:20:56 +0300

    change 404 page

--- client/src/screens/Page404/Page404.jsx
+++ client/src/screens/Page404/Page404.jsx
@@ -1,11 +1,18 @@
 import { Link } from 'react-router-dom';
+import Navbar from '../../components/NavbarMedium/NavbarMedium';
 import './Page404.css';
 
 const Page404 = () => {
 	return (
-		<Link to="/">
-			<div className="container-404"></div>
-		</Link>
+		<div className="container-404">
+			<Navbar />
+			<div className="page-404">Page 404</div>
+			<Link to="/">
+				<div className="page-404-go-home">
+					<span>Go Home</span>
+				</div>
+			</Link>
+		</div>
 	);
 };
 export default Page404;
