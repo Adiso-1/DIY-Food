86512715b7cbf11570d96a28a050fc6d73f57aba
Adi Levy <levyadi93@gmail.com>
2021-08-16 08:57:45 +0300

    delete redis to check deplyement

--- server/utils/init_redis.js
+++ server/utils/init_redis.js
@@ -1,31 +1,31 @@
-const Redis = require('redis');
+// const Redis = require('redis');
 
-const redisClient = Redis.createClient({
-	port: 6379,
-	host:
-		process.env.NODE_ENV === 'production'
-			? 'https://delicious-by-adi.herokuapp.com/'
-			: '127.0.0.1',
-});
+// const redisClient = Redis.createClient({
+// 	port: 6379,
+// 	host:
+// 		process.env.NODE_ENV === 'production'
+// 			? 'https://delicious-by-adi.herokuapp.com/'
+// 			: '127.0.0.1',
+// });
 
-redisClient.on('connect', () => {
-	console.log('redisClient connected to redis');
-});
+// redisClient.on('connect', () => {
+// 	console.log('redisClient connected to redis');
+// });
 
-redisClient.on('ready', () => {
-	console.log('redisClient connected to redis abnd ready to use');
-});
+// redisClient.on('ready', () => {
+// 	console.log('redisClient connected to redis abnd ready to use');
+// });
 
-redisClient.on('error', (err) => {
-	console.log(err.message);
-});
+// redisClient.on('error', (err) => {
+// 	console.log(err.message);
+// });
 
-redisClient.on('end', () => {
-	console.log('redisClient disconnected from redis');
-});
+// redisClient.on('end', () => {
+// 	console.log('redisClient disconnected from redis');
+// });
 
-process.on('SIGINT', () => {
-	redisClient.quit();
-});
+// process.on('SIGINT', () => {
+// 	redisClient.quit();
+// });
 
-module.exports = redisClient;
+// module.exports = redisClient;
